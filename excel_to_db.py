ee3cc1991ed504b7571f17cb13d53c50d615c1fa
nikthe883 <82037390+nikthe883@users.noreply.github.com>
2022-08-11 21:12:32 +0300

    First commit

--- excel_to_db.py
+++ excel_to_db.py
@@ -6,7 +6,7 @@ path = r"Databases\Matching_Philosophy\Decathlon_matching_philosophy_EN"
 
 def create_sql_database(filename_path):
     ''' Creates sqlite database from Excel file for matching philosophy for Decathlon'''
-    con = sqlite3.connect(f"{filename_path}.db")
+    con = sqlite3.connect(f"{filename_path}.sqlite")
     wb = pd.ExcelFile(filename_path + '.xlsx')
     for sheet in wb.sheet_names:
         df = pd.read_excel(filename_path + '.xlsx', sheet_name=sheet)
